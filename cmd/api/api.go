7bf17ccf9629a33ede88669b62ea1974c94157db
tedawf <hello@tedawf.com>
2024-10-14 02:46:52 +0800

    setup pq db

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,14 +7,24 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/tedawf/tt4d/internal/store"
 )
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 type application struct {
 	config config
+	store  store.Storage
 }
 
 func (app *application) mount() http.Handler {
