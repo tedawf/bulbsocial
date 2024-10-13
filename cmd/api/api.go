ffe3065a1e0684a020a3f1eb7df9f418fabc8e08
tedawf <hello@tedawf.com>
2024-10-13 22:21:18 +0800

    initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type config struct {
+	addr string
+}
+
+type application struct {
+	config config
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.handleHealthCheck)
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
