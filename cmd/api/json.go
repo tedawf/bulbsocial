2e8a2244b9ba58f154183679d2bcfc33d6867a19
tedawf <hello@tedawf.com>
2024-11-07 01:31:44 +0800

    fix error messages

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -16,7 +16,12 @@ func init() {
 func writeJSON(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(data)
+
+	if status != http.StatusNoContent {
+		return json.NewEncoder(w).Encode(data)
+	}
+
+	return nil
 }
 
 func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
