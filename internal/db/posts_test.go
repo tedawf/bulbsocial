6974e51cd7dedf6f33769b13a1081ce2b93f9b56
tedawf <hello@tedawf.com>
2024-12-01 19:05:52 +0800

    feat: add posts tests

--- internal/db/posts_test.go
+++ internal/db/posts_test.go
@@ -2,17 +2,17 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func CreateRandomPost(t *testing.T) Post {
-	user := CreateRandomTestUser(t)
+func CreateRandomTestPost(t *testing.T) Post {
 	arg := CreatePostParams{
 		Content: RandomContent(),
 		Title:   RandomTitle(),
-		UserID:  user.ID,
+		UserID:  CreateRandomTestUser(t).ID,
 		Tags:    RandomTags(),
 	}
 
@@ -29,5 +29,33 @@ func CreateRandomPost(t *testing.T) Post {
 }
 
 func TestCreatePost(t *testing.T) {
-	CreateRandomPost(t)
+	CreateRandomTestPost(t)
+}
+
+func TestDeletePost(t *testing.T) {
+	post1 := CreateRandomTestPost(t)
+	err := testQueries.DeletePost(context.Background(), post1.ID)
+	require.NoError(t, err)
+
+	post2, err := testQueries.GetUserByID(context.Background(), post1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, post2)
+}
+
+func TestUpdatePost(t *testing.T) {
+	post1 := CreateRandomTestPost(t)
+
+	arg := UpdatePostParams{
+		Title:   RandomTitle(),
+		Content: RandomContent(),
+		ID:      post1.ID,
+		Version: post1.Version,
+	}
+
+	version, err := testQueries.UpdatePost(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, version)
+
+	require.NotEqual(t, arg.Version, version)
 }
